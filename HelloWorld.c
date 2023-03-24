03a2a6bcfe5c6ffed53762dbdcad335edbd2a49f
geoffgeoffgeoff3 <2geoff@pobox.com>
2023-03-24 13:37:07 -0400

    Sorry Python

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,7 +6,7 @@ int main(void) {
 
     printf("Hello, lovely world\n");
     printf("Java sucks compared to C\n");
-    printf("Python really sucks compared to C\n");
+    printf("Python doesn't really suck compared to C\n");
 
     return(EXIT_SUCCESS);
 }
